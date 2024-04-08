ab91ce241d91b3a11932c879d4cda15c2ca75783
Michal Cengel <xcengel@stuba.sk>
2024-04-08 16:31:48 +0200

    Add project files.

---
+++ Zadanie_3_MichalCengel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zadanie_3_MichalCengel
+{
+    internal class Zadanie_3_MichalCengel
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
+    }
+}
